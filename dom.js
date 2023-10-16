48528feb846615054d5bedc1dd1c54c58ff2895a
Evendashiell <slowsteady0412@gmail.com>
2023-10-16 21:08:00 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,5 @@
+var headerTItle = document.getElementById('header-title');
+headerTItle.style.border = 'solid 3px #000';
+var Title = document.getElementById('title');
+Title.style.color = 'green';
+Title.style.fontWeight = 'bold';
