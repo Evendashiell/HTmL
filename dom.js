a8d414673be9008ce13c48255dee9576a7b58930
Evendashiell <slowsteady0412@gmail.com>
2023-10-16 21:44:00 +0530

    DOM2

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
-var headerTItle = document.getElementById('header-title');
-headerTItle.style.border = 'solid 3px #000';
-var Title = document.getElementById('title');
-Title.style.color = 'green';
-Title.style.fontWeight = 'bold';
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+for(var i = 0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
